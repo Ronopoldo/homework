12e492666aab25e1ff716b5ef35701c2e1a5916d
Ronopoldo <Ronopoldo@yandex.ru>
2022-11-28 17:45:16 +0300

    ОГЭ по информатике (за 2022 год)

---
+++ OGE-2022/EXAM/04.pas
@@ -0,0 +1,14 @@
+﻿var
+  num, amount: integer;
+
+begin
+  amount := 0;
+  num := -1;
+  while (num <> 0) do
+  begin
+    readLn(num);
+    if ((num <> 0) and (num mod 2 = 0) and (num mod 5 = 0))
+      then amount := amount + 1;
+  end;
+  writeLn(amount);
+end.
