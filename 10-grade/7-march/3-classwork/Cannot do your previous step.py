3d6678ffd35f81b6783a82d9c579ce8396e2b24a
Ronopoldo <Ronopoldo@yandex.ru>
2023-03-03 15:56:39 +0300

    Create Cannot do your previous step.py

---
+++ 10-grade/7-march/3-classwork/Cannot do your previous step.py
@@ -0,0 +1,17 @@
+def f(s,p0,p1,c,m):
+    if s>=121: return c%2==m%2
+    if c==m: return 0
+
+    h=[]
+    if p1!='+2': h+=[f(s+2,'+1',p0,c+1,m)]
+    if p1!='+5': h+=[f(s+5,'+2',p0,c+1,m)]
+    if p1!='*12': h+=[f(s*12,'*2',p0,c+1,m)]
+    if p1 != '*2': h += [f(s * 2, '*2', p0, c + 1, m)]
+
+    return any(h) if (c+1)%2==m%2 else any(h)
+
+
+for s in range(1,121):
+    for m in range(1,5):
+        if f(s,'','',0,m)==1:
+            print(s, m)
